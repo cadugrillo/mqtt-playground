1fd6fcd8012f17684e7b2597fe1aa8faa9150961
Cadu Grillo <cadugrillo@gmail.com>
2022-05-14 20:44:22 -0400

    added testing routine

--- subscriber/test/main.go
+++ subscriber/test/main.go
@@ -9,30 +9,16 @@ func main() {
 
 	b := mqttbuffer.Newmqttbuffer()
 
-	fmt.Println("Write pointer before adding new msg: ", b.GetWritePointer())
-	fmt.Println("Read pointer before adding new msg: ", b.GetReadPointer())
+	fmt.Println("Write pointer start value: ", b.GetWritePointer())
+	fmt.Println("Read pointer start value: ", b.GetReadPointer())
 
 	b = b.AddMessage("first message")
-	fmt.Println(b)
-	b = b.AddMessage("second message")
-	fmt.Println(b)
-	b = b.AddMessage("third message")
-	fmt.Println(b)
-	b = b.AddMessage("fourth message")
-	fmt.Println(b)
-	b = b.AddMessage("fifth message")
-	fmt.Println(b)
-	b = b.AddMessage("sixth message")
-	fmt.Println(b)
-
 	fmt.Println("Write pointer after adding new msg: ", b.GetWritePointer())
 	fmt.Println("Read pointer after adding new msg: ", b.GetReadPointer())
-
-	if b.GetReadPointer() < b.GetWritePointer() {
-		msg := b.ReadMessage(b.GetReadPointer())
-		fmt.Println("Received message: ", msg)
-		b = b.ReadNextMessage()
-	}
-	fmt.Println("Read pointer after reading next msg: ", b.GetReadPointer())
+	msg := b.ReadMessage(b.GetReadPointer())
+	fmt.Println("Received message: ", msg)
+	b = b.ReadNextMessage()
+	fmt.Println("Write pointer after adding new msg: ", b.GetWritePointer())
+	fmt.Println("Read pointer after adding new msg: ", b.GetReadPointer())
 
 }
